5e988ad0fd9e6c5c3dc2eaa80d4bc427dfb772df
Facundo Rodriguez <facurodriguezz76@gmail.com>
2025-09-14 18:36:07 -0300

    Se agrego el archivo test_dispositivo

---
+++ test/test_dispositvo.py
@@ -0,0 +1,24 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from modelos.dispositivo import Dispositivo
+from modelos.ubicacion import Ubicacion
+
+def test_cambiar_estado_dispositivo():
+    ubicacion = Ubicacion(1, "Cocina")
+
+    dispositivo = Dispositivo(
+        id_dispositivo=1,
+        nombre="Luz cocina",
+        esencial=True,
+        ubicacion=ubicacion
+    )
+
+    assert dispositivo.estado == "apagado"
+
+    dispositivo.cambiar_estado()
+    assert dispositivo.estado == "encendido"
+
+    dispositivo.cambiar_estado()
+    assert dispositivo.estado == "apagado"
