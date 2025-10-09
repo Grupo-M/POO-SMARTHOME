299ddc645e9809bc2a134fe50bddeaba27750d89
CarolinaLanfrancocarolinaalanfranco@gmail.com <carolina.a.lanfranco@gmail.com>
2025-10-09 12:13:04 -0300

    Agrego módulo db_conn.py para conexión y consultas MySQL

---
+++ conn/config_gral.py
@@ -0,0 +1,8 @@
+"Configuracion con base de datos"
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "root",
+    "password": "tu contraseña",
+    "database": "smarthome_db",
+    "port": 3306
+}
