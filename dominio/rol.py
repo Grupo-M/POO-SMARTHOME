25c03f15fabbd0e7a039901887a2a374fa4c7b6f
CarolinaLanfrancocarolinaalanfranco@gmail.com <carolina.a.lanfranco@gmail.com>
2025-10-18 16:29:57 -0300

    Encapsulamiento clase rol

--- dominio/rol.py
+++ dominio/rol.py
@@ -1,5 +1,34 @@
 class Rol:
     def __init__(self, id_rol: int, nombre: str, descripcion: str):
-        self.id_rol = id_rol
-        self.nombre = nombre
-        self.descripcion = descripcion
+        self.__id_rol = id_rol
+        self.__nombre = nombre
+        self.__descripcion = descripcion
+
+    @property
+    def id_rol(self) -> int:
+        return self.__id_rol
+    
+    @property
+    def nombre(self) -> str:
+        return self.__nombre
+    
+    @nombre.setter
+    def nombre(self, nuevo_nombre: str) -> None:
+        if not nuevo_nombre.strip():  
+            raise ValueError("El nombre del rol no puede estar vacío.")
+        self.__nombre = nuevo_nombre
+
+
+    @property
+    def descripcion(self) -> str:
+        return self.__descripcion
+    
+    @descripcion.setter
+    def descripcion(self, nueva_descripcion: str) -> None:
+        if not nueva_descripcion.strip():
+            raise ValueError("La descripción del rol no puede estar vacía.")
+        self.__descripcion = nueva_descripcion
+
+
+    def __str__(self) -> str:
+        return f"Rol {self.__nombre} (ID: {self.__id_rol}): {self.__descripcion}"
