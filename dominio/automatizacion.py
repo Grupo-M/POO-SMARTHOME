3314e69554ddb2e95a470f895b0bb0631a012a6d
CarolinaLanfrancocarolinaalanfranco@gmail.com <carolina.a.lanfranco@gmail.com>
2025-10-18 16:58:25 -0300

    Encapsulamiento clase automatizacion

--- dominio/automatizacion.py
+++ dominio/automatizacion.py
@@ -1,9 +1,20 @@
-class Automatizacion:
-    def __init__(self, nombre: str):
-        self.nombre = nombre
+from dominio.dispositivo import Dispositivo
 
-    def activar_modo_ahorro(self, lista_dispositivos):
+class Automatizacion:
+
+    def __init__(self, nombre: str):
+        if not nombre.strip():
+            raise ValueError("El nombre de la automatización no puede estar vacío.")
+        self.__nombre = nombre
+
+    @property
+    def nombre(self) -> str:
+        """Devuelve el nombre de la automatización."""
+        return self.__nombre
+
+    def activar_modo_ahorro(self, lista_dispositivos: list[Dispositivo]):
+      
         for dispositivo in lista_dispositivos:
-            dispositivo["estado"] = "apagado"
-        return lista_dispositivos
+            dispositivo.apagar()  
+        print(f"Modo ahorro activado para {len(lista_dispositivos)} dispositivos.")
 
